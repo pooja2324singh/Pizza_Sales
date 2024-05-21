f1efcbd5bbb5acc0a5449a159d774eba1b09d0eb
pooja2324singh <169133420+pooja2324singh@users.noreply.github.com>
2024-05-21 02:39:26 -0700

    Add files via upload

---
+++ Pizza_sales4.sql
@@ -0,0 +1,4 @@
+-- Most common pizza size ordered
+select pizzas.size, count(orders_details.order_details_id) as order_count
+from pizzas  join orders_details  on pizzas.pizza_id = orders_details.pizza_id
+group by pizzas.size;
